8fefbdd98a06960b97fb998fed68eb3db9cbb54f
phpdvlpmt <phpdvlpmt@gmail.com>
2020-01-06 10:31:53 +0100

    Add new Link

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,17 +1,24 @@
 <template>
-  <header
-    class="bg-blue-900 text-white py-3 px-3 md:px-10 shadow-md leading-none md:flex justify-between md:items-center"
-  >
+  <header class="bg-blue-900 text-white py-3 px-3 md:px-10 shadow-md leading-none md:flex justify-between md:items-center">
     <h1 class="text-center text-xs text-white tracking-wider">Učíme se online</h1>
     <nav class="flex justify-center md:m-0 mt-2 mb-2">
       <router-link to="/" class="text-center text-xs tracking-wider">Domů</router-link>
       <router-link to="/ceska-republika" class="text-center text-xs tracking-wider ml-3">ČR</router-link>
       <router-link to="/evropa" class="text-center text-xs tracking-wider ml-3">Evropa</router-link>
       <router-link to="/sousedni-staty" class="text-center text-xs tracking-wider ml-3">Sousední státy</router-link>
-     <!-- <router-link to="/staty" class="text-center text-xs tracking-wider ml-3">Státy</router-link>-->
+      <!-- <router-link to="/staty" class="text-center text-xs tracking-wider ml-3">Státy</router-link>-->
     </nav>
-
-    <h2 class="text-center text-xs text-white tracking-wider">Základní škola a mateřská škola Mnetěš</h2>
+    <div class="flex items-center">
+      <h2 class="text-center text-xs text-white tracking-wider">Základní škola a mateřská škola Mnetěš</h2>
+      <a class="text-center text-xs tracking-wider ml-3" href="http://mail.zsmsmnetes.cz" target="_blank">
+        <svg class="w-4 h-4 fill-current" viewBox="0 0 20 20">
+          <path
+            fill="#FFF"
+            d="M1.574 5.286l7.5 4.029c.252.135.578.199.906.199.328 0 .654-.064.906-.199l7.5-4.029c.489-.263.951-1.286.054-1.286H1.521c-.897 0-.435 1.023.053 1.286zm17.039 2.203l-7.727 4.027c-.34.178-.578.199-.906.199s-.566-.021-.906-.199-7.133-3.739-7.688-4.028C.996 7.284 1 7.523 1 7.707V15c0 .42.566 1 1 1h16c.434 0 1-.58 1-1V7.708c0-.184.004-.423-.387-.219z"
+          />
+        </svg>
+      </a>
+    </div>
   </header>
 </template>
 
