2d964eafc5a7987ae1d4eff0f2a7d5693bc9d0b9
phpdvlpmt <phpdvlpmt@gmail.com>
2020-01-20 11:13:51 +0100

    Add Slovakia

--- src/components/NStates.vue
+++ src/components/NStates.vue
@@ -1,6 +1,7 @@
 <template>
   <div>
     <div class="px-3 md:px-10">
+      <Slovakia></Slovakia>
       <Austria></Austria>
       <Germany></Germany>
     </div>
@@ -10,11 +11,13 @@
 <script>
 import Germany from "./Germany.vue";
 import Austria from "./Austria.vue";
+import Slovakia from "./Slovakia.vue";
 export default {
   name: "States",
   components: {
     Germany,
-    Austria
+    Austria,
+    Slovakia
   }
 };
 </script>
---
+++ src/components/Slovakia.vue
@@ -0,0 +1,153 @@
+<template>
+  <div id="slovakia">
+    <div class="pt-6">
+      <div class="md:flex justify-between items-center mb-4">
+        <div>
+          <h1 class="text-gray-800 text-xl font-semibold text-center md:text-left md:text-xl">Slovenská republika (Slovensko) - hospodářské, kulturní a přátelské vztahy</h1>
+        </div>
+        <div>
+          <img class="h-auto py-2 md:py-0 md:h-10 mx-auto" src="https://upload.wikimedia.org/wikipedia/commons/thumb/e/e6/Flag_of_Slovakia.svg/110px-Flag_of_Slovakia.svg.png" alt="" />
+        </div>
+      </div>
+      <!--/flag-->
+      <div class="sm:flex sm:flex-wrap -mx-2">
+        <div class="w-full sm:w-1/2 lg:w-1/4 px-2 mb-4 flex flex-col">
+          <div class="rounded overflow-hidden shadow-lg flex-grow ">
+            <div class="overflow-hidden">
+              <img
+                class="w-full h-40 object-cover object-center scale hover:cursor-zoom-in"
+                src="https://upload.wikimedia.org/wikipedia/commons/thumb/1/1d/Bratislava_Panorama_R01.jpg/1024px-Bratislava_Panorama_R01.jpg"
+                alt="Braniborská brána"
+              />
+            </div>
+            <div class="px-6 pt-1 pb-4">
+              <h2 class="font-bold text-lg">Hlavní město: <strong>Bratislava</strong></h2>
+              <p class="text-gray-600 text-sm">
+                Hlavním městem protéká řeka Dunaj, nad městem se tyčí Bratislavský hrad. Na obrázku vidíte Bratislavu.
+                <a class="underline text-blue-600 text-xs" target="_blank" href="https://commons.wikimedia.org/w/index.php?curid=24883875">
+                  Autor: Marc Ryckaert (MJJR) – Vlastní dílo, CC BY 3.0,
+                </a>
+              </p>
+              <h2 class="font-bold text-lg">Z historie</h2>
+              <p class="text-gray-600 text-sm">
+                Slovenská republika, stejně jako Česká republika vznikla <strong>1. ledna 1993</strong> po rozdělení Československa. Se Slovenskem máme dlouholeté kulturní a hospodářské vazby, které
+                se utvořily v letech, kdy jsme žili v jednom státě. Na Slovensku se bez potíží domluvíme česky. Mnoho Čechů dodnes žije na Slovensku a naopak mnoho Slováků u nás.
+              </p>
+              <h2 class="font-bold text-lg">Obyvatelstvo</h2>
+              <p class="text-gray-600 text-sm">Na Slovensku žije asi <strong>5 milionů obyvatel</strong>.</p>
+              <h2 class="font-bold text-lg">Další velká města</h2>
+              <p class="text-gray-600 text-sm">
+                Košice, Prešov, Banská Bystrica, Žilina, Nitra, Trenčín
+              </p>
+            </div>
+          </div>
+        </div>
+        <!--/1st-->
+        <div class="w-full sm:w-1/2 lg:w-1/4 px-2 mb-4 flex flex-col">
+          <div class="rounded overflow-hidden shadow-lg flex-grow">
+            <div class="overflow-hidden">
+              <img
+                class="w-full h-40 object-cover object-center scale hover:cursor-zoom-in"
+                src="https://upload.wikimedia.org/wikipedia/commons/thumb/3/3a/Vysok%C3%A9_Tatry%2C_v%C3%BDhled_z_vrcholu_Rysy%2C_z%C3%A1%C5%99%C3%AD_2011_%2829%29.JPG/800px-Vysok%C3%A9_Tatry%2C_v%C3%BDhled_z_vrcholu_Rysy%2C_z%C3%A1%C5%99%C3%AD_2011_%2829%29.JPG"
+                alt=""
+              />
+            </div>
+            <div class="px-6 pt-1 pb-2">
+              <h2 class="font-bold text-lg">Povrch</h2>
+              <p class="text-gray-600 text-sm">
+                Území Slovenska je převážně hornaté. Většinu povrchu zaujímá pohoří <strong>Karpaty</strong>. V jeho části nazvané <strong>Vysoké Tatry</strong> leží nejvyšší horaSlovenska
+                <strong>Gerlachovský štít</strong> (2655 m n.m.). Nejvíc lidí žije v kotlinách pod horami a v nížinách na jihu a východě země. Na obrázku vidíte Vysoké Tatry.
+                <a class="underline text-blue-600 text-xs" target="_blank" href="https://commons.wikimedia.org/w/index.php?curid=16952118">Autor: Michal Klajban – Vlastní dílo, CC BY-SA 3.0</a>
+              </p>
+
+              <h2 class="font-bold text-lg">Vodstvo</h2>
+              <p class="text-gray-600 text-sm">
+                Podunajskou nižinou protéká řeka <strong>Dunaj</strong>, která tvoří přirozeou hranici s Maďarskem. Nejdelší řekou je však <strong>Váh</strong>, na němž je vybudováno mnoho vodních
+                elektráren. Většina jezer je ledovcového původu, říká se jim plesa. Na obrázku dole vidíte řeku Váh.
+                <a class="underline text-blue-600 text-xs" target="_blank" href=" https://commons.wikimedia.org/w/index.php?curid=27812057">Autor: Jerzy Opioła – Vlastní dílo, CC BY-SA 3.0,</a>
+              </p>
+            </div>
+            <div class="overflow-hidden">
+              <img
+                class="w-full h-40 object-cover object-center scale hover:cursor-zoom-in"
+                src="https://upload.wikimedia.org/wikipedia/commons/thumb/d/d5/Hrad_Stre%C4%8Dno_2_MF3.jpg/800px-Hrad_Stre%C4%8Dno_2_MF3.jpg"
+                alt=""
+              />
+            </div>
+          </div>
+        </div>
+        <!--/2-->
+        <div class="w-full sm:w-1/2 lg:w-1/4 px-2 mb-4 flex flex-col">
+          <div class="rounded overflow-hidden shadow-lg flex-grow">
+            <div class="overflow-hidden">
+              <img
+                class="w-full h-40 object-cover object-center scale hover:cursor-zoom-in"
+                src="https://upload.wikimedia.org/wikipedia/commons/thumb/4/46/Refinery_of_Slovnaft%2C_view_from_Nov%C3%BD_most_viewpoint_in_Bratislava%2C_Bratislava_II_District.jpg/1024px-Refinery_of_Slovnaft%2C_view_from_Nov%C3%BD_most_viewpoint_in_Bratislava%2C_Bratislava_II_District.jpg"
+                alt=""
+              />
+            </div>
+            <div class="px-6 pt-1 pb-4">
+              <h2 class="font-bold text-lg">Zemědělství</h2>
+              <p class="text-gray-600 text-sm">
+                V nížinách se pěstují teplomilné rostliny (<strong>kukuřice, pšenice, ovoce, zelenina</strong>). Na horských loukách se pasou <strong>ovce</strong> a <strong>krávy</strong>. Na
+                Slovensku je mnoho lesů, proto zde vznikl <strong>dřevozpracující průmysl</strong>.
+              </p>
+              <h2 class="font-bold text-lg">Průmysl</h2>
+              <p class="text-gray-600 text-sm">
+                V období po roce 1945 se Slovensko stalo průmyslově vyspělým státem s rozvinutým <strong>hutnictvím</strong> (Košice) a <strong>chemickým průmyslem</strong> (Bratislava). Slovensko je
+                členem <strong>NATO</strong> a <strong>EU</strong>. Na obrázku vidíte Bratislavskou rafinérii.
+                <a class="underline text-blue-600" target="_blank" href="https://commons.wikimedia.org/w/index.php?curid=7650023">Autor: Frettie – Vlastní dílo, CC BY 3.0</a>
+              </p>
+              <img class="mt-8 w-20 mx-auto" src="https://upload.wikimedia.org/wikipedia/commons/thumb/d/d2/Coat_of_arms_of_Slovakia.svg/800px-Coat_of_arms_of_Slovakia.svg.png" alt="" />
+              <p>
+                <a class="underline text-blue-600 text-xs" target="_lank" href="https://commons.wikimedia.org/w/index.php?curid=846468"
+                  >Autor: Tlusťa – Čl. 9. ods. 1 Ústavy Slovenskej republiky, § 2, ods. 1 zákona NR SR č. 63/1993 Z. z. o štátnych symboloch Slovenskej republiky a ich používaní, Volné dílo,</a
+                >
+              </p>
+            </div>
+          </div>
+        </div>
+        <!--/3-->
+
+        <div class="w-full sm:w-1/2 lg:w-1/4 px-2 mb-4 flex flex-col">
+          <div class="rounded overflow-hidden shadow-lg flex-grow">
+            <div class="overflow-hidden">
+              <img
+                class="w-full h-40 object-cover object-center scale hover:cursor-zoom-in"
+                src="https://upload.wikimedia.org/wikipedia/commons/thumb/0/09/Bojnice%2C_j%C3%BAn_2014_-_panoramio.jpg/800px-Bojnice%2C_j%C3%BAn_2014_-_panoramio.jpg"
+                alt=""
+              />
+            </div>
+            <div class="px-6 pt-1 pb-4">
+              <h2 class="font-bold text-lg">Turistika</h2>
+              <p class="text-gray-600 text-xs italic">
+                Na obrázku vidíte Bojnický zámek.
+                <a class="underline text-blue-600" target="_blank" href="https://commons.wikimedia.org/w/index.php?curid=58812548">Autor: Janobb, CC BY 3.0</a> Turisté směřují nejčastěji do horských
+                oblastí Slovenska. Nejvíce navštěvované jsou <strong>Vysoké Tatry</strong> a <strong>Nízké Tatry</strong>. Vyhledávaná jsou také města s histrickými památkami: Bratislava, Košice,
+                Nitra, Trenčín, Banská Štiavnica.
+              </p>
+              <p class="text-gray-600 text-xs">
+                Na Slovensku je mnoho krásných zámků (např. <strong>zámek Bojnice</strong> nedaleko Prievidze a <strong>Spišský hrad</strong> poblíž Prešova). Turisticky vyhledávané jsou krasové
+                oblasti <strong>Slovenský kras</strong> a <strong>Demänovské jeskyně</strong>. Hodně navštěvované jsou slovenské <strong>lázně s termálními prameny</strong> (např. Piešťany,
+                Trenčianské Teplice). Oblíbená je i rekreační oblast v okolí vodní nádrže <strong>Zemplínnska Šírava</strong> ve Východoslovenské nížině.
+              </p>
+
+              <p class="text-gray-600 text-xs">
+                Více informací o Slovensku můžete najít třeba na <a class="text-blue-600 underline" href="https://cs.wikipedia.org/wiki/Slovensko" target="_blank">Wikipedii</a>.
+              </p>
+              <h2 class="font-bold text-lg">Shrnutí</h2>
+              <p class="text-gray-600 text-sm">
+                <strong>
+                  Slovenská republika byla do roku 1993 součástí Československa. Žije zde asi 5 milionů obyvatel. Území je většinou hornaté. Turisty lákají velehory Vysoké Tatry, ale také hlavní město
+                  Bratislava ležící v Podunajské nížině. Nejznámějšími řekami jsou Dunaj a Váh.
+                </strong>
+              </p>
+            </div>
+          </div>
+        </div>
+        <!--/4-->
+      </div>
+      <!--end 1 row-->
+    </div>
+  </div>
+</template>
